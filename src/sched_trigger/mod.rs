b324500ebee0d51bee1380854b903f03bef65ed4
kazuki0824 <kazukuro0824@gmail.com>
2022-09-12 01:06:44 +0900

    sched_trigger

--- src/sched_trigger/mod.rs
+++ src/sched_trigger/mod.rs
@@ -1,29 +1,33 @@
 use std::cell::Cell;
+use std::ops::DerefMut;
 use std::sync::Mutex;
 use chrono::{Local, DateTime};
 use tokio::sync::mpsc::Sender;
+use uuid::Uuid;
 use crate::recording_pool::RecordControlMessage;
 
 pub(crate) static Q_RESERVED: Mutex<Vec<Reservation>> = Mutex::new(vec![]);
 
+#[derive(Clone)]
 pub(crate) struct Reservation {
     start: Cell<DateTime<Local>>,
-    end: Cell<DateTime<Local>>
-
+    end: Cell<DateTime<Local>>,
+    auto: Option<Uuid> // If it is added auto-planned reservation (e.g. Record all of the items in the series)
 }
 
 pub(crate) async fn scheduler_startup(tx: Sender<RecordControlMessage>) -> ! {
 
     loop {
-        let lock = Q_RESERVED.lock().unwrap();
+        let mut lock = Q_RESERVED.lock().unwrap();
 
+        let mut v = lock.clone();
         for item in lock.iter() {
             if is_in_the_recording_range(item.start.get(), item.end.get(), Local::now()) {
                 tx.send(RecordControlMessage::Add).await.unwrap();
-            } else if item.end.get() < Local::now() {
-                tx.send(RecordControlMessage::Remove).await.unwrap()
             }
         }
+        // Drop expired item TODO: Repetitive recording rule
+        lock.retain(|item| item.end.get() < Local::now());
 
         tokio::time::sleep(std::time::Duration::from_secs(5)).await;
     }
