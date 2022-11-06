887ffba7f5daad49ffc32ea87a5c44cd29de749a
kazuki0824 <kazukuro0824@gmail.com>
2022-11-07 01:06:07 +0900

    reverse_event_relay for frontend interfacing

--- src/epg_syncer/periodic_tasks.rs
+++ src/epg_syncer/periodic_tasks.rs
@@ -1,5 +1,8 @@
+use std::collections::HashMap;
 use log::info;
 use meilisearch_sdk::errors::Error;
+use mirakurun_client::models::Program;
+use mirakurun_client::models::related_item::Type;
 
 use crate::db_utils::{push_programs_ranges, push_services_ranges};
 use crate::epg_syncer::EpgSyncManager;
@@ -12,6 +15,20 @@ impl EpgSyncManager {
         let p = fetch_programmes(&self.m_conf).await;
         let s = fetch_services(&self.m_conf).await;
         (s, p)
+    }
+    async fn get_reverse_event_relay(p: &Vec<Program>) -> HashMap<i32, i32>
+    {
+        let mut table = HashMap::new();
+        for elem in p {
+            if let Some(ref rels) = elem.related_items {
+                for r in rels {
+                    if let Some(Type::Relay) = r.r#type {
+                        table.insert(r.event_id.unwrap(),elem.event_id);
+                    }
+                }
+            }
+        }
+        table
     }
     pub(crate) async fn refresh_db(&self) -> Result<(), Error> {
         // Periodically updates the list of currently available channels, future programs.
